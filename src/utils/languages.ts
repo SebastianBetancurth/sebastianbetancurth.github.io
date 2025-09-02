dea01c1fb8a42a43dccbc938a20aa59d117378f6
Sebastian Betancurth <33584221+SebastianBetancurth@users.noreply.github.com>
2025-09-02 13:54:42 -0300

    update

--- src/utils/languages.ts
+++ src/utils/languages.ts
@@ -29,7 +29,7 @@ export const languages: Record<string, Language> = {
     iconName: "mysql",
   },
   shopify: {
-    name: "Shopfy",
+    name: "Shopify",
     iconName: "shopify",
   },
   node: {
